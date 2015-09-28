047fa5bcaa9c0d1e4911d57409be52f1fcf22d72
Eric Berquist <eric.berquist@gmail.com>
2015-09-28 11:17:06 -0400

    Finish simple-minded code to read in 2- and 4-index integrals.

--- eric/project3/project3.py
+++ eric/project3/project3.py
@@ -1,23 +1,66 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+from __future__ import division
 
 import numpy as np
 
-# 1. How to open integral files?
+from ..utils import print_mat
 
-dim = 7
-filename_overlap = "h2o_sto3g_s.dat"
+def parse_file_1(filename):
+    with open(filename) as fh:
+        val = float(fh.readline())
+    return val
 
 def parse_int_file_2(filename, dim):
     mat = np.zeros(shape=(dim, dim))
-    with open(filename_overlap) as fh:
+    with open(filename) as fh:
         contents = fh.readlines()
     for line in contents:
-        sline = line.split()
-        mu, nu, intval = map(float, sline)
+        mu, nu, intval = map(float, line.split())
         mat[mu-1, nu-1] = mat[nu-1, mu-1] = intval
     return mat
 
-mat = parse_int_file_2(filename_overlap, dim)
-print(mat)
+def parse_int_file_4(filename, dim):
+    # be very inefficient with how we store these for now -- use all 4
+    # indices
+    mat = np.zeros(shape=(dim, dim, dim, dim))
+    with open(filename) as fh:
+        contents = fh.readlines()
+    for line in contents:
+        mu, nu, lm, sg, intval = map(float, line.split())
+        mu, nu, lm, sg = mu - 1, nu - 1, lm - 1, sg - 1
+        mat[mu, nu, lm, sg] = \
+            mat[mu, nu, sg, lm] = \
+            mat[nu, mu, lm, sg] = \
+            mat[nu, mu, sg, lm] = \
+            mat[lm, sg, mu, nu] = \
+            mat[lm, sg, nu, mu] = \
+            mat[sg, lm, mu, nu] = \
+            mat[sg, lm, nu, mu] = intval
+    return mat
+
+nelec = 10
+dim = 7
+
+stub = "h2o_sto3g_"
+
+filename_enuc = stub + "enuc.dat"
+filename_s = stub + "s.dat"
+filename_t = stub + "t.dat"
+filename_v = stub + "v.dat"
+filename_eri = stub + "eri.dat"
+
+enuc = parse_file_1(filename_enuc)
+mat_s = parse_int_file_2(filename_s, dim)
+mat_t = parse_int_file_2(filename_t, dim)
+mat_v = parse_int_file_2(filename_v, dim)
+mat_eri = parse_int_file_4(filename_eri, dim)
+
+print("Nuclear repulsion energy = {}\n".format(enuc))
+print("Overlap Integrals:")
+print_mat(mat_s)
+print("Kinetic-Energy Integrals:")
+print_mat(mat_t)
+print("Nuclear Attraction Integrals:")
+print_mat(mat_v)
