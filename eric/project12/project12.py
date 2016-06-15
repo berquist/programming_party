a53ad5f2b2151c0abc1c8c5b1e35f164a61204c4
Eric Berquist <eric.berquist@gmail.com>
2016-06-15 18:22:35 -0400

    Add DALTON results for excitation energies.

--- eric/project12/project12.py
+++ eric/project12/project12.py
@@ -8,6 +8,8 @@ import numpy as np
 from ..utils import print_mat
 from ..utils import np_load
 
+from ..project3.project3 import parse_int_file_2
+
 
 def getargs():
 
@@ -56,18 +58,23 @@ def build_fock_spin_orbital(TEI_SO, H, nsocc):
 
 
 def make_spin_orbital_energies(E_MO):
+    """Place the molecular orbital energies in a diagonal matrix indexed
+    identially to the spin-orbital basis quantities.
+    """
+
     N_MO = E_MO.shape[0]
     N_SO = N_MO * 2
     E_SO = np.zeros((N_SO))
+
     for p in range(N_SO):
         E_SO[p] = E_MO[p//2, p//2]
+
     return np.diag(E_SO)
 
 
-def form_hamiltonian_cis(H_CIS, E_SO, TEI_SO, nsocc):
-    """Form the CIS Hamiltonian in the spin orbital basis."""
+def form_hamiltonian_cis_so(H_CIS, E_SO, TEI_SO, nsocc):
+    """Form the CIS Hamiltonian in the spin orbital (SO) basis."""
 
-    # nsov = H_CIS.shape[0]
     nsorb = E_SO.shape[0]
     nsvir = nsorb - nsocc
 
@@ -77,14 +84,13 @@ def form_hamiltonian_cis(H_CIS, E_SO, TEI_SO, nsocc):
             for j in range(nsocc):
                 for b in range(nsvir):
                     jb = j*nsvir + b
-                    # print('i: {} a: {} ia: {} j: {} b: {} jb: {}'.format(i, a, ia, j, b, jb))
                     H_CIS[ia, jb] = ((i == j) * E_SO[a + nsocc, b + nsocc]) - ((a == b) * E_SO[i, j]) + TEI_SO[a + nsocc, j, i, b + nsocc]
 
     return
 
 
-def form_hamiltonian_cis_singlet(H_CIS_singlet, E_MO, TEI_MO, nocc):
-    """Form the singlet CIS Hamiltonian in the molecular orbital basis."""
+def form_hamiltonian_cis_mo_singlet(H_CIS_singlet, E_MO, TEI_MO, nocc):
+    """Form the singlet CIS Hamiltonian in the molecular orbital (MO) basis."""
 
     norb = E_MO.shape[0]
     nvirt = norb - nocc
@@ -101,8 +107,8 @@ def form_hamiltonian_cis_singlet(H_CIS_singlet, E_MO, TEI_MO, nocc):
     return
 
 
-def form_hamiltonian_cis_triplet(H_CIS_triplet, E_MO, TEI_MO, nocc):
-    """Form the triplet CIS Hamiltonian in the molecular orbital basis."""
+def form_hamiltonian_cis_mo_triplet(H_CIS_triplet, E_MO, TEI_MO, nocc):
+    """Form the triplet CIS Hamiltonian in the molecular orbital (MO) basis."""
 
     norb = E_MO.shape[0]
     nvirt = norb - nocc
@@ -119,50 +125,8 @@ def form_hamiltonian_cis_triplet(H_CIS_triplet, E_MO, TEI_MO, nocc):
     return
 
 
-# def form_rpa_a_matrix(E_MO, TEI_MO):
-#     """Form the A (CIS) matrix for RPA."""
-
-#     norb = TEI_MO.shape[0]
-#     nvirt = norb - nocc
-#     nov = nocc * nvirt
-
-#     A = np.empty(shape=(nov, nov))
-
-#     for i in range(nocc):
-#         for a in range(nvirt):
-#             ia = i*nvirt + a
-#             for j in range(nocc):
-#                 for b in range(nvirt):
-#                     jb = j*nvirt + b
-#                     # <aj||ib> = <aj|ib> - <aj|bi> = (ai|jb) - (ab|ji)
-#                     A[ia, jb] = ((i == j) * E_MO[a + nocc, b + nocc]) - ((a == b) * E_MO[i, j]) + TEI_MO[a + nocc, i, j, b + nocc] - TEI_MO[a + nocc, b + nocc, j, i]
-
-#     return A
-
-
-# def form_rpa_b_matrix(TEI_MO):
-#     """Form the B matrix for RPA."""
-
-#     norb = TEI_MO.shape[0]
-#     nvirt = norb - nocc
-#     nov = nocc * nvirt
-
-#     B = np.empty(shape=(nov, nov))
-
-#     for i in range(nocc):
-#         for a in range(nvirt):
-#             ia = i*nvirt + a
-#             for j in range(nocc):
-#                 for b in range(nvirt):
-#                     jb = j*nvirt + b
-#                     # <ab||ij> = <ab|ij> - <ab|ji> = (ai|bj) - (aj|bi)
-#                     B[ia, jb] = TEI_MO[a + nocc, i, b + nocc, j] - TEI_MO[a + nocc, j, b + nocc, i]
-
-#     return B
-
-
 def form_rpa_a_matrix_so(E_SO, TEI_SO, nocc):
-    """Form the A (CIS) matrix for RPA in the spin orbital basis."""
+    """Form the A (CIS) matrix for RPA in the spin orbital (SO) basis."""
 
     norb = TEI_SO.shape[0]
     nvirt = norb - nocc
@@ -176,13 +140,35 @@ def form_rpa_a_matrix_so(E_SO, TEI_SO, nocc):
             for j in range(nocc):
                 for b in range(nvirt):
                     jb = j*nvirt + b
+                    # <aj||ib>
                     A[ia, jb] = ((i == j) * E_SO[a + nocc, b + nocc]) - ((a == b) * E_SO[i, j]) + TEI_SO[a + nocc, j, i, b + nocc]
 
     return A
 
 
+def form_rpa_a_matrix_mo(E_MO, TEI_MO, nocc):
+    """Form the A (CIS) matrix for RPA in the molecular orbital (MO) basis."""
+
+    norb = E_MO.shape[0]
+    nvirt = norb - nocc
+    nov = nocc * nvirt
+
+    A = np.empty(shape=(nov, nov))
+
+    for i in range(nocc):
+        for a in range(nvirt):
+            ia = i*nvirt + a
+            for j in range(nocc):
+                for b in range(nvirt):
+                    jb = j*nvirt + b
+                    # <aj||ib> = (ai||jb) = (ai|jb) - (ab|ji)
+                    # A[ia, jb] = ((i == j) * E_SO[a + nocc, b + nocc]) - ((a == b) * E_SO[i, j]) + TEI_SO[a + nocc, j, i, b + nocc]
+                    A[ia, jb] = ((i == j) * E_MO[a + nocc, b + nocc]) - ((a == b) * E_MO[i, j]) + TEI_MO[a + nocc, i, j, b + nocc] - TEI_MO[a + nocc, b + nocc, j, i]
+
+    return A
+
 def form_rpa_b_matrix_so(TEI_SO, nocc):
-    """Form the B matrix for RPA in the spin orbital basis."""
+    """Form the B matrix for RPA in the spin orbital (SO) basis."""
 
     norb = TEI_SO.shape[0]
     nvirt = norb - nocc
@@ -201,6 +187,158 @@ def form_rpa_b_matrix_so(TEI_SO, nocc):
     return B
 
 
+def form_rpa_b_matrix_mo(TEI_MO, nocc):
+    """Form the B matrix for RPA in the molecular orbital (MO) basis."""
+
+    norb = TEI_MO.shape[0]
+    nvirt = norb - nocc
+    nov = nocc * nvirt
+
+    B = np.empty(shape=(nov, nov))
+
+    for i in range(nocc):
+        for a in range(nvirt):
+            ia = i*nvirt + a
+            for j in range(nocc):
+                for b in range(nvirt):
+                    jb = j*nvirt + b
+                    # <ab||ij> = <ab|ij> - <ab|ji>
+                    # <ab||ij> = (ai||bj) = (ai|bj) - (aj|bi)
+                    B[ia, jb] = TEI_MO[a + nocc, i, b + nocc, j] - TEI_MO[a + nocc, j, b + nocc, i]
+
+    return B
+
+
+def davidson_jjgoings():
+    # dimension of matrix
+    n = 1200
+    # convergence tolerance
+    tol = 1e-8
+    # maximum number of iterations
+    mmax = n // 2
+
+    # Create sparse, diagonally-dominant matrix A with diagonal
+    # containing 1,2,3,...n. The eigenvalues should be very close to
+    # these values. You can change the sparsity. A smaller number for
+    # sparsity increases the diagonal dominance. Larger values
+    # (e.g. sparsity = 1) create a dense matrix.
+    sparsity = 1e-4
+    A = np.diag(np.arange(1, n + 1, dtype=float))
+    A += sparsity * np.random.randn(n, n)
+    A = (A.T + A) / 2
+
+    # number of initial guess vectors
+    k = 8
+    # number of eigenvalues to solve for
+    eig = 4
+    # set of k unit vectors as guess
+    t = np.eye(n, k)
+    # array of zeros to hold guess vec
+    V = np.zeros((n, n))
+    # identity matrix same dimen as A
+    I = np.eye(n)
+
+    # begin block Davidson routine
+
+    import time
+
+    start_davidson = time.time()
+
+    for m in range(k, mmax, k):
+        if m <= k:
+            for j in range(k):
+                V[:, j] = t[:, j] / np.linalg.norm(t[:, j])
+            theta_old = 1
+        elif m > k:
+            theta_old = theta[:eig]
+        V, _ = np.linalg.qr(V)
+        T = np.dot(V[:, :(m + 1)].T, np.dot(A, V[:, :(m + 1)]))
+        THETA, S = np.linalg.eig(T)
+        idx = THETA.argsort()
+        theta = THETA[idx]
+        s = S[:, idx]
+        for j in range(k):
+            w = np.dot((A - theta[j] * I), np.dot(V[:, :(m + 1)], s[:, j]))
+            q = w / (theta[j] - A[j, j])
+            V[:, :(m + j + 1)] = q
+        norm = np.linalg.norm(theta[:eig] - theta_old)
+        if norm < tol:
+            break
+
+    end_davidson = time.time()
+
+    # end of block Davidson, print results
+
+    print("davidson = {}; {} seconds".format(theta[:eig], end_davidson - start_davidson))
+
+    # begin NumPy diagonalization of A
+
+    start_numpy = time.time()
+
+    E, Vec = np.linalg.eig(A)
+    E = np.sort(E)
+
+    end_numpy = time.time()
+
+    # end of NumPy diagonalization, print results
+
+    print("numpy = {}; {} seconds".format(theta[:eig], end_numpy - start_numpy))
+
+    return
+
+
+def form_cis_difference_density_mo(eigvec, nocc, norb):
+    """"""
+
+    P_CIS_DD_MO = np.zeros(shape=(norb, norb))
+
+    nvirt = norb - nocc
+
+    # occ-occ
+    l1 = []
+    for i in range(nocc):
+        for j in range(nocc):
+            for a in range(nvirt):
+                ia = i*nvirt + a
+                for b in range(nvirt):
+                    jb = j*nvirt + b
+                    l1.append((ia, jb))
+                    # P_CIS_DD_MO[i, j] += (eigvec[ia] * eigvec[jb])
+    # virt-virt
+    # for a in range(nvirt):
+    #     for b in range(nvirt):
+    #         for i in range(nocc):
+    #             ia = i*nvirt + a
+    #             for j in range(nocc):
+    #                 jb = j*nvirt + b
+                    # P_CIS_DD_MO[a, b] -= (eigvec[ia] * eigvec[jb])
+
+    l2 = []
+    for i in range(0, nocc):
+        for j in range(0, nocc):
+            for a in range(nocc, norb):
+                ia = i*nvirt + a - nocc
+                for b in range(nocc, norb):
+                    jb = j*nvirt + b - nocc
+                    l2.append((ia, jb))
+                    P_CIS_DD_MO[i, j] += (eigvec[ia] * eigvec[jb])
+                    P_CIS_DD_MO[a, b] -= (eigvec[ia] * eigvec[jb])
+
+    assert l1 == l2
+    # occ-virt is zero
+
+    return P_CIS_DD_MO
+
+
+def form_cis_difference_density_ao(P_CIS_DD_MO, C):
+
+    assert P_CIS_DD_MO.shape[0] == P_CIS_DD_MO.shape[1] == C.shape[1]
+
+    P_CIS_DD_AO = np.dot(C, np.dot(P_CIS_DD_MO, C.T))
+
+    return P_CIS_DD_AO
+
+
 if __name__ == "__main__":
 
     args = getargs()
@@ -211,6 +349,7 @@ if __name__ == "__main__":
     nvirt = norb - nocc
     nov = nocc * nvirt
 
+    C = np_load('C.npz')
     E_MO = np_load('F_MO.npz')
     H = np_load('H.npz')
     TEI_MO = np_load('TEI_MO.npz')
@@ -235,11 +374,11 @@ if __name__ == "__main__":
 
     H_CIS = np.zeros(shape=(nsov, nsov))
     # Form the CIS Hamiltonian.
-    form_hamiltonian_cis(H_CIS, E_SO, TEI_SO, nsocc)
+    form_hamiltonian_cis_so(H_CIS, E_SO, TEI_SO, nsocc)
     energies_CIS, eigvecs_CIS = np.linalg.eigh(H_CIS)
     idx_CIS = energies_CIS.argsort()
     energies_CIS = energies_CIS[idx_CIS]
-    eigvecs_CIS = eigvecs_CIS[idx_CIS]
+    eigvecs_CIS = eigvecs_CIS[:, idx_CIS]
     print('CIS excitation energies (SO basis)')
     hartree_to_ev = 27.211385
     for i, e in enumerate(energies_CIS, start=1):
@@ -248,10 +387,16 @@ if __name__ == "__main__":
     ## Spin-adapted CIS
     H_CIS_singlet = np.zeros(shape=(nov, nov))
     H_CIS_triplet = np.zeros(shape=(nov, nov))
-    form_hamiltonian_cis_singlet(H_CIS_singlet, E_MO, TEI_MO, nocc)
-    form_hamiltonian_cis_triplet(H_CIS_triplet, E_MO, TEI_MO, nocc)
+    form_hamiltonian_cis_mo_singlet(H_CIS_singlet, E_MO, TEI_MO, nocc)
+    form_hamiltonian_cis_mo_triplet(H_CIS_triplet, E_MO, TEI_MO, nocc)
     energies_CIS_singlet, eigvecs_CIS_singlet = np.linalg.eigh(H_CIS_singlet)
     energies_CIS_triplet, eigvecs_CIS_triplet = np.linalg.eigh(H_CIS_triplet)
+    idx_CIS_singlet = energies_CIS_singlet.argsort()
+    idx_CIS_triplet = energies_CIS_triplet.argsort()
+    energies_CIS_singlet = energies_CIS_singlet[idx_CIS_singlet]
+    energies_CIS_triplet = energies_CIS_triplet[idx_CIS_triplet]
+    eigvecs_CIS_singlet = eigvecs_CIS_singlet[:, idx_CIS_singlet]
+    eigvecs_CIS_triplet = eigvecs_CIS_triplet[:, idx_CIS_triplet]
     _energies_CIS_singlet = sorted(((e, 'singlet') for e in energies_CIS_singlet))
     _energies_CIS_triplet = sorted(((e, 'triplet') for e in energies_CIS_triplet))
     energies_CIS = sorted(_energies_CIS_singlet + _energies_CIS_triplet)
@@ -259,7 +404,41 @@ if __name__ == "__main__":
     for i, (e, t) in enumerate(energies_CIS, start=1):
         print(i, e, e * hartree_to_ev, t)
 
+    # print('HF (GS) electronic dipole')
+    # M100_AO = parse_int_file_2(args.stub + '_mux.dat', dim)
+    # M010_AO = parse_int_file_2(args.stub + '_muy.dat', dim)
+    # M001_AO = parse_int_file_2(args.stub + '_muz.dat', dim)
+    # print('dipole X AO')
+    # print_mat(M100_AO)
+    # print('dipole Y AO')
+    # print_mat(M010_AO)
+    # print('dipole Z AO')
+    # print_mat(M001_AO)
+    # Form the HF density in the AO basis.
+    # P_HF_AO = np.dot(C[:, :nocc], C[:, :nocc].T)
+    # print('MO coefficients')
+    # print_mat(C)
+    # print('HF density matrix')
+    # print_mat(P_HF_AO)
+    # dipole_HF_elec_au = -np.array([np.sum(P_HF_AO * M100_AO), np.sum(P_HF_AO * M010_AO), np.sum(P_HF_AO * M001_AO)])
+    # convfac_au_to_debye = 2.541746230211
+    # dipole_HF_elec_debye = convfac_au_to_debye * dipole_HF_elec_au
+    # print('(a.u.): {:f} {:f} {:f}'.format(*dipole_HF_elec_au))
+    # print('(D)   : {:f} {:f} {:f}'.format(*dipole_HF_elec_debye))
+    # print('CIS electronic dipole')
+    # Form the CIS difference density matrix in the MO basis.
+    # P_CIS_DD_MO = form_cis_difference_density_mo(eigvecs_CIS_singlet[:, 0], nocc, norb)
+    # Form the CIS difference density matrix in the AO basis.
+    # P_CIS_DD_AO = form_cis_difference_density_ao(P_CIS_DD_MO, C)
+    # Form the CIS density in the AO basis.
+    # P_CIS_AO = P_HF_AO + P_CIS_DD_AO
+    # dipole_CIS_elec_au = -np.array([np.sum(P_CIS_AO * M100_AO), np.sum(P_CIS_AO * M010_AO), np.sum(P_CIS_AO * M001_AO)])
+    # dipole_CIS_elec_debye = convfac_au_to_debye * dipole_CIS_elec_au
+    # print('(a.u.): {:f} {:f} {:f}'.format(*dipole_CIS_elec_au))
+    # print('(D)   : {:f} {:f} {:f}'.format(*dipole_CIS_elec_debye))
+
     ## Time-Dependent Hartree-Fock (TDHF) / Random Phase Approximation (RPA)
+    ## method 1
     A = form_rpa_a_matrix_so(E_SO, TEI_SO, nsocc)
     B = form_rpa_b_matrix_so(TEI_SO, nsocc)
     # Form the RPA supermatrix.
@@ -273,6 +452,7 @@ if __name__ == "__main__":
     for i, e in enumerate(eigvals_RPA, start=1):
         print(i, e, e * hartree_to_ev)
 
+    ## method 2
     H_RPA_reduced = np.dot(A + B, A - B)
     eigvals_RPA_reduced, eigvecs_RPA_reduced = np.linalg.eig(H_RPA_reduced)
     idx_RPA_reduced = eigvals_RPA_reduced.argsort()
@@ -281,3 +461,89 @@ if __name__ == "__main__":
     print('RPA excitation energies (SO basis), method 2')
     for i, e in enumerate(eigvals_RPA_reduced, start=1):
         print(i, e, e * hartree_to_ev)
+
+    ## method 1, MO basis
+    A = form_rpa_a_matrix_mo(E_MO, TEI_MO, nocc)
+    B = form_rpa_b_matrix_mo(TEI_MO, nocc)
+    # Form the RPA supermatrix.
+    H_RPA_MO = np.bmat([[ A,  B],
+                        [-B, -A]])
+    eigvals_RPA_MO, eigvecs_RPA_MO = np.linalg.eig(H_RPA_MO)
+    idx_RPA_MO = eigvals_RPA_MO.argsort()
+    eigvals_RPA_MO = eigvals_RPA_MO[idx_RPA_MO].real
+    eigvecs_RPA_MO = eigvecs_RPA_MO[idx_RPA_MO].real
+    print('RPA excitation energies (MO basis), method 1')
+    for i, e in enumerate(eigvals_RPA_MO, start=1):
+        print(i, e, e * hartree_to_ev)
+
+    # davidson_jjgoings()
+
+    ## Davidson-Liu algorithm for spin-adapted singlet CIS
+    # counter = 1
+    # conv = 1.0e30
+    # thresh = 1.0e-4
+    # H_dim = H_CIS_singlet.shape[0]
+    # nroots = 3
+    # L = nroots * 2
+    # I = np.eye(H_dim)
+    # # while conv < thresh:
+    # while counter < 4:
+    #     print('iter: {}'.format(counter))
+    #     # 1. Select guess vectors.
+    #     guess_vectors = np.eye(L, H_dim)
+    #     print('guess vectors:')
+    #     print(guess_vectors)
+    #     # 2. Build and diagonalize the subspace Hamiltonian.
+    #     G = np.empty(shape=(L, L))
+    #     sigmas = []
+    #     for j in range(L):
+    #         sigma = np.dot(H_CIS_singlet, guess_vectors[j])
+    #         sigmas.append(sigma)
+    #         for i in range(L):
+    #             G[i, j] = np.dot(guess_vectors[i], sigmas[j])
+    #     print('sigma vectors:')
+    #     print(np.array(sigmas))
+    #     print('subspace Hamiltonian:')
+    #     print(G)
+    #     eigvals_lambda, eigvecs_alpha = np.linalg.eigh(G)
+    #     print('subspace Hamiltonian eigenvalues:')
+    #     print(eigvals_lambda)
+    #     print('subspace Hamiltonian eigenvectors:')
+    #     print(eigvecs_alpha)
+    #     # sort and only keep the desired number of roots from the subspace
+    #     idx = eigvals_lambda.argsort()[:nroots]
+    #     eigvals_lambda = eigvals_lambda[idx]
+    #     eigvecs_alpha = eigvecs_alpha[:, idx]
+    #     # This is the current estimate of each of the eigenvectors as
+    #     # a linear combination of the guess vectors with the subspace
+    #     # eigenvectors {k} providing the coefficients.
+    #     c = np.empty(shape=(L, nroots))
+    #     for k in range(nroots):
+    #         for j in range(L):
+    #             for i in range(L):
+    #                 c[j, k] = eigvecs_alpha[i, k] * guess_vectors[j, i]
+    #         # c_k = [eigvecs_alpha[i, k] * guess_vectors[i, :] for i in range(L)]
+    #         # print(c_k)
+    #         # c[:, k] = [eigvecs_alpha[i, k] * guess_vectors[i].T for i in range(L)]
+    #         # for i in range(L):
+    #         #     c[i, k] = eigvecs_alpha[i, k] * guess_vectors[k]
+    #     print('current estimate')
+    #     print(c)
+    #     # 3. Build the correction vectors.
+    #     residuals = np.empty(shape=(L, nroots))
+    #     for k in range(nroots):
+    #         for i in range(L):
+    #             residual = eigvecs_alpha[i, k] * np.dot((H_CIS_singlet - I * eigvals_lambda[k]), guess_vectors[i])
+    #             # residual2 = eigvecs_alpha[i, k] * (sigmas[i] - eigvals_lambda[k] * guess_vectors[i])
+    #             residuals[i, k] = residual
+    #     print('residuals:')
+    #     print(residuals)
+    #     correction_vectors_delta = np.empty(shape=(L, L))
+    #     for I in range(L):
+    #         for k in range(L):
+    #             pass
+    #             # correction_vectors_delta[I, k] = (eigvals_lambda[k] - H_CIS_singlet[I, I])**(-1) * residuals[I][k]
+    #     # 4. Orthonormalize the correction vectors by first
+    #     # normalizing them then orthogonalizing them against the guess
+    #     # vectors.
+    #     counter += 1
