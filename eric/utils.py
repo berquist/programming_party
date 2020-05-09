a77be5aa926afa658424ad171bf8e913d8bf036d
Eric Berquist <eric.berquist@gmail.com>
2020-05-08 23:43:23 -0400

    Add type annotations and f-strings to Python utils

--- eric/utils.py
+++ eric/utils.py
@@ -1,57 +1,55 @@
-#!/usr/bin/env python
+import sys
 
 import numpy as np
 
 
-def idx2(i, j):
+def idx2(i: int, j: int) -> int:
     """Returns the compound index for two values.
     """
-
     if i > j:
-        return (i*(i+1))/2 + j
+        return int((i * (i + 1)) / 2 + j)
     else:
-        return (j*(j+1))/2 + i
+        return int((j * (j + 1)) / 2 + i)
 
-def idx4(i, j, k, l):
+
+def idx4(i: int, j: int, k: int, l: int) -> int:
     """Returns the compound index for four values.
     """
+    return idx2(idx2(i, j), idx2(k, l))
 
-    ij = idx2(i, j)
-    kl = idx2(k, l)
-    return idx2(ij, kl)
 
-def print_mat(mat):
+def print_mat(mat: np.ndarray) -> None:
     """Pretty-print a general NumPy matrix in a traditional format.
     """
-
     dim_rows, dim_cols = mat.shape
     # first, handle the column labels
-    print(' ' * 5)
+    print(" " * 5)
     for i in range(dim_cols):
-        print('{0:12d}'.format(i+1), end='')
-    print('')
+        print(f"{i + 1:12d}", end="")
+    print("")
     # then, handle the row labels
     for i in range(dim_rows):
-        print('{0:5d}'.format(i+1), end='')
+        print(f"{i + 1:5d}", end="")
         # print the matrix data
         for j in range(dim_cols):
-            print('{0:12.7f}'.format(mat[i][j]), end='')
-        print('', end='\n')
-    print('', end='\n')
+            print(f"{mat[i][j]:12.7f}", end="")
+        print("", end="\n")
+    print("", end="\n")
 
     return
 
-def np_load(filename):
+
+def np_load(filename: str) -> np.ndarray:
     arr = np.load(filename)
     if isinstance(arr, np.lib.npyio.NpzFile):
         # Make the assumption that there's only a single array
         # present, even though *.npz files can hold multiple
         # arrays.
-        arr = arr.items()[0][1]
+        arr = list(arr.items())[0][1]
     return arr
 
 
-def read_arma_mat_ascii(armaasciifilename):
+def read_arma_mat_ascii(armaasciifilename: str) -> np.ndarray:
     """Given a file name, read it in as an ASCII-formatted Armadillo matrix.
 
     Currently, it supports matrices and cubes.
@@ -85,7 +83,8 @@ def read_arma_mat_ascii(armaasciifilename):
 
     return arma_mat
 
-def matsym(amat, thrzer=1.0e-14):
+
+def matsym(amat: np.ndarray, thrzer: float = 1.0e-14) -> bool:
     """This function returns
        1 if the matrix is symmetric to threshold THRZER
        2 if the matrix is antisymmetric to threshold THRZER
@@ -103,9 +102,8 @@ def matsym(amat, thrzer=1.0e-14):
     isym = 1
     iasym = 2
     for j in range(n):
-        # for i in range(j+1):
         # The +1 is so the diagonal elements are checked.
-        for i in range(j+1):
+        for i in range(j + 1):
             amats = abs(amat[i, j] + amat[j, i])
             amata = abs(amat[i, j] - amat[j, i])
             if amats > thrzer:
@@ -113,4 +111,4 @@ def matsym(amat, thrzer=1.0e-14):
             if amata > thrzer:
                 isym = 0
 
-    return (isym + iasym)
+    return bool(isym + iasym)
